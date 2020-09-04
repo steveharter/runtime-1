80ee5c8e158ea3cc7f4376e9a3e36df906b1f151
Steve Harter <steveharter@users.noreply.github.com>
2020-09-04 13:35:59 -0500

    Test existing dynamic behavior and prototype new converters

---
+++ src/libraries/System.Text.Json/tests/Serialization/CustomConverterTests/CustomConverterSample.Dynamic.cs
@@ -0,0 +1,344 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
+
+namespace System.Text.Json.Serialization.Tests
+{
+    /// <summary>
+    /// Used to represent a JSON object.
+    /// </summary>
+    internal class JsonObject : Dictionary<string, object>
+    {
+        // todo: implement IDictionary<string, object> and forward to Dictionary<string, object>
+
+        /// <summary>
+        /// A shortcut to obtain an array value.
+        /// </summary>
+        /// <param name="propertyName">The name of the property.</param>
+        /// <returns></returns>
+        public JsonArray GetArray(string propertyName)
+        {
+            return (JsonArray)this[propertyName];
+        }
+
+        /// <summary>
+        /// A shortcut to obtain an array value.
+        /// </summary>
+        /// <param name="propertyName">The name of the property.</param>
+        /// <returns></returns>
+        public JsonObject GetObject(string propertyName)
+        {
+            return (JsonObject)this[propertyName];
+        }
+    }
+
+    /// <summary>
+    /// Used to represent a JSON array.
+    /// </summary>
+    internal class JsonArray : List<object>
+    {
+        // todo: implement IList<object> and forward to List<object>.
+
+        /// <summary>
+        /// A shortcut to obtain an array value.
+        /// </summary>
+        /// <param name="propertyName">The name of the property.</param>
+        /// <returns></returns>
+        public JsonArray GetArray(int index)
+        {
+            return (JsonArray)this[index];
+        }
+
+        /// <summary>
+        /// A shortcut to obtain an array value.
+        /// </summary>
+        /// <param name="propertyName">The name of the property.</param>
+        /// <returns></returns>
+        public JsonObject GetObject(int index)
+        {
+            return (JsonObject)this[index];
+        }
+    }
+
+    /// <summary>
+    /// Determines how JSON is deserialized when the target type is <see cref="object"/>.
+    /// </summary>
+    internal enum SystemObjectDeserializationPolicy
+    {
+        /// <summary>
+        /// The default behavior. <see cref="System.Text.Json.JsonElement"/> is deserialized for all JSON except <see cref="System.Text.Json.JsonTokenType.Null"/>.
+        /// For <see cref="System.Text.Json.JsonTokenType.Null"/>, <see langword="null"/> is returned.
+        /// This supports an explicit model where the caller needs to know the specific primitive types.
+        /// </summary>
+        JsonElement = 0,
+
+        /// <summary>
+        /// JSON objects support <see langword="dynamic"/>.
+        /// JSON primitives use <see cref="System.Text.Json.JsonElement"/>.
+        /// JSON arrays use JsonArray.
+        /// Note this loads the assembly "System.Text.RegularExpressions.dll".
+        /// This supports an explicit model where the caller needs to know the specific primitive type.
+        /// For <see cref="System.Text.Json.JsonTokenType.StartObject"/>, <see cref="System.Dynamic.ExpandoObject"/> is returned
+        /// which allows for usage of the <see langword="dynamic"/> feature.
+        /// For <see cref="System.Text.Json.JsonTokenType.StartArray"/>, <see cref="System.Collections.IList<object>"/> is returned.
+        /// For <see cref="System.Text.Json.JsonTokenType.Null"/>, <see langword="null"/> is returned.
+        /// For all other tokens, <see cref="System.Text.Json.JsonElement"/> is returned.
+        /// </summary>
+        JsonElementDynamic = 1,
+
+        /// <summary>
+        /// JSON objects use JsonObject.
+        /// JSON primitives are converted to their respective types. This mapping is not exact.
+        /// JSON arrays use JsonArray.
+        /// <see cref="System.Text.Json.JsonElement"/> is not used.
+        /// For <see cref="System.Text.Json.JsonTokenType.StartObject"/>, <see cref="JsonObject"/> is returned.
+        /// For <see cref="System.Text.Json.JsonTokenType.StartArray"/>, <see cref="JsonArray"/> is returned.
+        /// For <see cref="System.Text.Json.JsonTokenType.String"/>, <see cref="System.DateTime"/> is returned if possible otherwise <see cref="string"/> is returned.
+        /// For <see cref="System.Text.Json.JsonTokenType.Number"/>, <see cref="System.Int64"/> is returned if possible otherwise <see cref="System.Double"/> is returned.
+        /// For <see cref="System.Text.Json.JsonTokenType.True"/> and <see cref="System.Text.Json.JsonTokenType.False"/>, <see cref="System.Boolean"/> is returned.
+        /// For <see cref="System.Text.Json.JsonTokenType.Null"/>, <see langword="null"/> is returned.
+        /// </summary>
+        AutoConversion = 2,
+
+        /// <summary>
+        /// JSON objects support <see langword="dynamic"/>.
+        /// JSON primitives are converted to their respective types. This mapping is not exact.
+        /// JSON arrays use JsonArray.
+        /// Note this loads the assembly "System.Text.RegularExpressions.dll".
+        /// <see cref="System.Text.Json.JsonElement"/> is not used.
+        /// For <see cref="System.Text.Json.JsonTokenType.StartObject"/>, <see cref="System.Dynamic.ExpandoObject"/> is returned
+        /// which allows for usage of the <see langword="dynamic"/> feature.
+        /// For <see cref="System.Text.Json.JsonTokenType.StartArray"/>, <see cref="JsonArray"/> is returned.
+        /// For <see cref="System.Text.Json.JsonTokenType.String"/>, <see cref="System.DateTime"/> is returned if possible otherwise <see cref="string"/> is returned.
+        /// For <see cref="System.Text.Json.JsonTokenType.Number"/>, <see cref="System.Int64"/> is returned if possible otherwise <see cref="System.Double"/> is returned.
+        /// For <see cref="System.Text.Json.JsonTokenType.True"/> and <see cref="System.Text.Json.JsonTokenType.False"/>, <see cref="System.Boolean"/> is returned.
+        /// For <see cref="System.Text.Json.JsonTokenType.Null"/>, <see langword="null"/> is returned.
+        /// </summary>
+        AutoConversionDynamic = 3
+    }
+
+    internal static class JsonSerializerExtensions
+    {
+        public static void SetSystemObjectDeserializationPolicy(this JsonSerializerOptions options, SystemObjectDeserializationPolicy value)
+        {
+            if (value == SystemObjectDeserializationPolicy.JsonElementDynamic ||
+                value == SystemObjectDeserializationPolicy.AutoConversionDynamic)
+            {
+                options.Converters.Add(new ExpandoObjectConverter());
+            }
+
+
+            if (value != SystemObjectDeserializationPolicy.JsonElement)
+            {
+                options.Converters.Add(new SystemObjectConverter(value));
+            }
+        }
+    }
+
+
+    internal sealed class SystemObjectConverter : JsonConverter<object>
+    {
+        private SystemObjectDeserializationPolicy _policy;
+        JsonConverter<JsonArray> JsonArrayConverter { get; set; }
+        JsonConverter<ExpandoObject> ExpandoObjectConverter { get; set; }
+        JsonConverter<JsonObject> JsonObjectConverter { get; set; }
+
+        public SystemObjectConverter(SystemObjectDeserializationPolicy policy)
+        {
+            if (policy != SystemObjectDeserializationPolicy.JsonElementDynamic &&
+                policy != SystemObjectDeserializationPolicy.AutoConversion &&
+                policy != SystemObjectDeserializationPolicy.AutoConversionDynamic)
+            {
+                throw new NotSupportedException($"SystemObjectDeserializationPolicy enum value '{policy}'not known.");
+            }
+
+            _policy = policy;
+        }
+
+        public override sealed object Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                return GetString(ref reader, options);
+            }
+
+            if (reader.TokenType == JsonTokenType.Number)
+            {
+                return GetNumber(ref reader, options);
+            }
+
+            if (reader.TokenType == JsonTokenType.StartArray)
+            {
+                return GetArray(ref reader, options);
+            }
+
+            if (reader.TokenType == JsonTokenType.StartObject)
+            {
+                return GetObject(ref reader, options);
+            }
+
+            if (reader.TokenType == JsonTokenType.False)
+            {
+                return GetFalse(ref reader, options);
+            }
+
+            if (reader.TokenType == JsonTokenType.True)
+            {
+                return GetTrue(ref reader, options);
+            }
+
+            Debug.Fail("Unexpected token type.");
+            throw new JsonException();
+        }
+
+        private object GetArray(ref Utf8JsonReader reader, JsonSerializerOptions options)
+        {
+            if (JsonArrayConverter == null)
+            {
+                JsonArrayConverter = (JsonConverter<JsonArray>)options.GetConverter(typeof(JsonArray));
+            }
+
+            return JsonArrayConverter.Read(ref reader, typeof(JsonArray), options);
+        }
+
+        private object GetNumber(ref Utf8JsonReader reader, JsonSerializerOptions options)
+        {
+            if (_policy == SystemObjectDeserializationPolicy.JsonElementDynamic)
+            {
+                return GetJsonElement(ref reader, options);
+            }
+
+            // Ideally we call into the configured Int64 converter, but there is no "Try" mechanism.
+            if (reader.TryGetInt64(out long l))
+            {
+                return l;
+            }
+
+            return reader.GetDouble();
+        }
+
+        private object GetString(ref Utf8JsonReader reader, JsonSerializerOptions options)
+        {
+            if (_policy == SystemObjectDeserializationPolicy.JsonElementDynamic)
+            {
+                return GetJsonElement(ref reader, options);
+            }
+
+            // Ideally we call into the configured DateTime converter, but there is no "Try" mechanism.
+            if (reader.TryGetDateTime(out DateTime datetime))
+            {
+                return datetime;
+            }
+
+            return reader.GetString();
+        }
+
+        private object GetObject(ref Utf8JsonReader reader, JsonSerializerOptions options)
+        {
+            if (_policy == SystemObjectDeserializationPolicy.AutoConversion)
+            {
+                if (JsonObjectConverter == null)
+                {
+                    JsonObjectConverter = (JsonConverter<JsonObject>)options.GetConverter(typeof(JsonObject));
+                }
+
+                return JsonObjectConverter.Read(ref reader, typeof(JsonObject), options);
+            }
+
+            if (ExpandoObjectConverter == null)
+            {
+                ExpandoObjectConverter = (JsonConverter<ExpandoObject>)options.GetConverter(typeof(ExpandoObject));
+            }
+
+            // This will either be a configured ExpandoObject converter, or the default implementation of IDictionary<string, object>.
+
+            return ExpandoObjectConverter.Read(ref reader, typeof(ExpandoObject), options);
+        }
+
+        private object GetFalse(ref Utf8JsonReader reader, JsonSerializerOptions options)
+        {
+            if (_policy == SystemObjectDeserializationPolicy.JsonElementDynamic)
+            {
+                return GetJsonElement(ref reader, options);
+            }
+
+            return false;
+        }
+
+        private object GetTrue(ref Utf8JsonReader reader, JsonSerializerOptions options)
+        {
+            if (_policy == SystemObjectDeserializationPolicy.JsonElementDynamic)
+            {
+                return GetJsonElement(ref reader, options);
+            }
+
+            return true;
+        }
+
+        private object GetJsonElement(ref Utf8JsonReader reader, JsonSerializerOptions options)
+        {
+            using (JsonDocument document = JsonDocument.ParseValue(ref reader))
+            {
+                return document.RootElement.Clone();
+            }
+        }
+
+        public override sealed void Write(Utf8JsonWriter writer, object value, JsonSerializerOptions options)
+        {
+            throw new InvalidOperationException("Should not get here.");
+        }
+    }
+
+
+    internal sealed class ExpandoObjectConverter : JsonConverter<ExpandoObject>
+    {
+        private JsonConverter<IDictionary<string, object>> Converter { get; set; }
+        private JsonSerializerOptions _options;
+
+        JsonConverter<IDictionary<string, object>> GetConverter(JsonSerializerOptions options)
+        {
+            if (Converter == null)
+            {
+                _options = options;
+                Converter = (JsonConverter<IDictionary<string, object>>)options.GetConverter(typeof(IDictionary<string, object>));
+            }
+            else if (_options != options)
+            {
+                throw new JsonException("ExpandoObjectConverter should not be shared across JsonSerializerOptions instances.");
+            }
+
+            return Converter;
+        }
+
+        public override sealed ExpandoObject Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType != JsonTokenType.StartObject)
+            {
+                throw new JsonException();
+            }
+
+            ExpandoObject expando = new ExpandoObject();
+            IDictionary<string, object> expandoDictionary = expando;
+
+            // Deserialize into Dictionary and then copy the values to the expando instance.
+            // We could write a loop to process each property that would avoid the temporary Dictionary instance,
+            // but that would have issues with reporting Path in JsonException as well as skip fast-path branches.
+            IDictionary<string, object> allProps = GetConverter(options).Read(ref reader, typeof(IDictionary<string, object>), options);
+            foreach (KeyValuePair<string, object> item in allProps)
+            {
+                expandoDictionary[item.Key] = item.Value;
+            }
+
+            return expando;
+        }
+
+        public override void Write(Utf8JsonWriter writer, ExpandoObject value, JsonSerializerOptions options)
+        {
+            GetConverter(options).Write(writer, value, options);
+        }
+    }
+}
---
+++ src/libraries/System.Text.Json/tests/Serialization/CustomConverterTests/CustomConverterSample.DynamicTests.cs
@@ -0,0 +1,244 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using Xunit;
+
+namespace System.Text.Json.Serialization.Tests
+{
+    public static class DynamicTests_CustomConverters
+    {
+        [Fact]
+        public static void SystemObjectDeserializationPolicy_JsonElementDynamic()
+        {
+            var options = new JsonSerializerOptions();
+            options.SetSystemObjectDeserializationPolicy(SystemObjectDeserializationPolicy.JsonElementDynamic);
+
+            dynamic obj = JsonSerializer.Deserialize<dynamic>(DynamicTests_DefaultConverters.Json, options);
+            Assert.IsType<ExpandoObject>(obj);
+
+            VerifyPrimitives();
+            VerifyObject();
+            VerifyArray();
+
+            void VerifyPrimitives()
+            {
+                // Primitives use JsonElement.
+                Assert.Equal("Hello", obj.MyString.GetString());
+                Assert.True(obj.MyBoolean.GetBoolean());
+                Assert.Equal(42, obj.MyInt.GetInt32());
+                Assert.Equal(4.2, obj.MyDouble.GetDouble());
+                Assert.Equal(DynamicTests_DefaultConverters.MyDateTime, obj.MyDateTime.GetDateTime());
+            }
+
+            void VerifyObject()
+            {
+                Assert.IsType<ExpandoObject>(obj.MyObject);
+                Assert.Equal("World", obj.MyObject.MyString.GetString());
+            }
+
+            void VerifyArray()
+            {
+                Assert.IsType<JsonArray>(obj.MyArray);
+                Assert.Equal(2, obj.MyArray.Count);
+                Assert.Equal(1, obj.MyArray[0].GetInt32());
+                Assert.Equal(2, obj.MyArray[1].GetInt32());
+            }
+        }
+
+        [Fact]
+        public static void SystemObjectDeserializationPolicy_AutoConversion()
+        {
+            var options = new JsonSerializerOptions();
+            options.SetSystemObjectDeserializationPolicy(SystemObjectDeserializationPolicy.AutoConversion);
+
+            VerifyAutoConvertPrimitives(options);
+            VerifyAutoConvertArray(options);
+
+            // Although we could use 'dynamic' here to wrap first-level properties,
+            // the intent of using AutoConversion is to avoid dynamic code.
+            object obj = JsonSerializer.Deserialize<object>(DynamicTests_DefaultConverters.Json, options);
+            Assert.IsAssignableFrom<JsonObject>(obj);
+            var jsonObj = (JsonObject)obj;
+
+            VerifyPrimitives();
+            VerifyObject();
+            VerifyArray();
+
+            void VerifyPrimitives()
+            {
+                // Primitives are auto-converted.
+                Assert.Equal("Hello", jsonObj["MyString"]);
+                Assert.Equal(true, jsonObj["MyBoolean"]);
+                Assert.Equal(42L, jsonObj["MyInt"]);
+                Assert.Equal(4.2, jsonObj["MyDouble"]);
+                Assert.Equal(DynamicTests_DefaultConverters.MyDateTime, jsonObj["MyDateTime"]);
+            }
+
+            void VerifyObject()
+            {
+                Assert.IsType<JsonObject>(jsonObj.GetObject("MyObject"));
+                Assert.Equal("World", jsonObj.GetObject("MyObject")["MyString"]);
+            }
+
+            void VerifyArray()
+            {
+                Assert.IsType<JsonArray>(jsonObj.GetArray("MyArray"));
+                Assert.Equal(2, jsonObj.GetArray("MyArray").Count);
+                Assert.Equal(1L, jsonObj.GetArray("MyArray")[0]);
+                Assert.Equal(2L, jsonObj.GetArray("MyArray")[1]);
+            }
+        }
+
+        [Fact]
+        public static void SystemObjectDeserializationPolicy_AutoConversionDynamic()
+        {
+            var options = new JsonSerializerOptions();
+            options.SetSystemObjectDeserializationPolicy(SystemObjectDeserializationPolicy.AutoConversionDynamic);
+
+            VerifyAutoConvertPrimitives(options);
+            VerifyAutoConvertArray(options);
+
+            dynamic obj = JsonSerializer.Deserialize<dynamic>(DynamicTests_DefaultConverters.Json, options);
+            Assert.IsType<ExpandoObject>(obj);
+
+            VerifyPrimitives();
+            VerifyObject();
+            VerifyArray();
+
+            void VerifyPrimitives()
+            {
+                // Primitives are auto-converted.
+                Assert.Equal("Hello", obj.MyString);
+                Assert.True(obj.MyBoolean);
+                Assert.Equal(42L, obj.MyInt);
+                Assert.Equal(4.2, obj.MyDouble);
+                Assert.Equal(DynamicTests_DefaultConverters.MyDateTime, obj.MyDateTime);
+            }
+
+            void VerifyObject()
+            {
+                Assert.IsType<ExpandoObject>(obj.MyObject);
+                Assert.Equal("World", obj.MyObject.MyString);
+            }
+
+            void VerifyArray()
+            {
+                Assert.IsType<JsonArray>(obj.MyArray);
+                Assert.Equal(2, obj.MyArray.Count);
+                Assert.Equal(1, obj.MyArray[0]);
+                Assert.Equal(2, obj.MyArray[1]);
+            }
+        }
+
+        private static void VerifyAutoConvertPrimitives(JsonSerializerOptions options)
+        {
+            {
+                const string Value = @"null";
+
+                object obj = JsonSerializer.Deserialize<object>(Value, options);
+                Assert.Null(obj);
+
+                object newtonsoftObj = Newtonsoft.Json.JsonConvert.DeserializeObject<object>(Value);
+                Assert.Null(newtonsoftObj);
+            }
+
+            {
+                const string Value = @"""mystring""";
+
+                object obj = JsonSerializer.Deserialize<object>(Value, options);
+                Assert.IsType<string>(obj);
+                Assert.Equal("mystring", obj);
+
+                object newtonsoftObj = Newtonsoft.Json.JsonConvert.DeserializeObject<object>(Value);
+                Assert.IsType<string>(newtonsoftObj);
+                Assert.Equal(newtonsoftObj, obj);
+            }
+
+            {
+                const string Value = "true";
+
+                object obj = JsonSerializer.Deserialize<object>(Value, options);
+                Assert.IsType<bool>(obj);
+                Assert.True((bool)obj);
+
+                object newtonsoftObj = Newtonsoft.Json.JsonConvert.DeserializeObject<object>(Value);
+                Assert.IsType<bool>(newtonsoftObj);
+                Assert.Equal(newtonsoftObj, obj);
+            }
+
+            {
+                const string Value = "false";
+
+                object obj = JsonSerializer.Deserialize<object>(Value, options);
+                Assert.IsType<bool>(obj);
+                Assert.False((bool)obj);
+
+                object newtonsoftObj = Newtonsoft.Json.JsonConvert.DeserializeObject<object>(Value);
+                Assert.IsType<bool>(newtonsoftObj);
+                Assert.Equal(newtonsoftObj, obj);
+            }
+
+            {
+                const string Value = "123";
+
+                object obj = JsonSerializer.Deserialize<object>(Value, options);
+                Assert.IsType<long>(obj);
+                Assert.Equal((long)123, obj);
+
+                object newtonsoftObj = Newtonsoft.Json.JsonConvert.DeserializeObject<object>(Value);
+                Assert.IsType<long>(newtonsoftObj);
+                Assert.Equal(newtonsoftObj, obj);
+            }
+
+            {
+                const string Value = "123.45";
+
+                object obj = JsonSerializer.Deserialize<object>(Value, options);
+                Assert.IsType<double>(obj);
+                Assert.Equal(123.45d, obj);
+
+                object newtonsoftObj = Newtonsoft.Json.JsonConvert.DeserializeObject<object>(Value);
+                Assert.IsType<double>(newtonsoftObj);
+                Assert.Equal(newtonsoftObj, obj);
+            }
+
+            {
+                const string Value = @"""2019-01-30T12:01:02Z""";
+
+                object obj = JsonSerializer.Deserialize<object>(Value, options);
+                Assert.IsType<DateTime>(obj);
+                Assert.Equal(new DateTime(2019, 1, 30, 12, 1, 2, DateTimeKind.Utc), obj);
+
+                object newtonsoftObj = Newtonsoft.Json.JsonConvert.DeserializeObject<object>(Value);
+                Assert.IsType<DateTime>(newtonsoftObj);
+                Assert.Equal(newtonsoftObj, obj);
+            }
+
+            {
+                const string Value = @"""2019-01-30T12:01:02+01:00""";
+
+                object obj = JsonSerializer.Deserialize<object>(Value, options);
+                Assert.IsType<DateTime>(obj);
+
+                object newtonsoftObj = Newtonsoft.Json.JsonConvert.DeserializeObject<object>(Value);
+                Assert.IsType<DateTime>(newtonsoftObj);
+                Assert.Equal(newtonsoftObj, obj);
+            }
+        }
+
+        private static void VerifyAutoConvertArray(JsonSerializerOptions options)
+        {
+            const string Value = "[]";
+
+            object obj = JsonSerializer.Deserialize<object>(Value, options);
+            Assert.IsAssignableFrom<IList<object>>(obj);
+
+            // Types are different.
+            object newtonsoftObj = Newtonsoft.Json.JsonConvert.DeserializeObject<object>(Value);
+            Assert.IsType<Newtonsoft.Json.Linq.JArray>(newtonsoftObj);
+        }
+    }
+}
